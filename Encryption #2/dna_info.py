280127651d034b1092ef81f6b48c85f53520cc1a
Alex-Shane <ashane1230@gmail.com>
2023-01-06 17:42:14 -0800

    Started second encryption project. Currently writing helper function to turn binary representation into nucleotide base

---
+++ Encryption #2/dna_info.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  6 17:32:35 2023
+
+@author: Alex
+"""
+
+#Alex Shane, ashane4
+
+def encode_sequence (message):
+    for letter in message:
+        binary_letter = bin(ord(letter))
+        #call helper function to turn binary form of letter into dna letter
+        #print dna letter
+
+def binary_to_nucleotide (num):
+    for pair in range 
