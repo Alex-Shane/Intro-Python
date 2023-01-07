76c675866ee3f469fe1ae53eec77ac097180456a
Alex-Shane <ashane1230@gmail.com>
2023-01-06 18:41:49 -0800

    added comments

--- Encryption #2/dna_info.py
+++ Encryption #2/dna_info.py
@@ -10,15 +10,19 @@ Created on Fri Jan  6 17:32:35 2023
 
 def encode_sequence (message):
     encoded = ""
+    #loop through every letter in message, turning it into a set of 4 dna bases
     for letter in message:
+        #transform letter into binary representation
         letter_in_binary = format(ord(letter), '08b')
-        #print (letter_in_binary)
+        #call helper function to create 4 dna bases from binary rep. add result
+        #to overall encoded message
         encoded += binary_to_nucleotide (letter_in_binary)
     print (encoded)
-    print (encoded == "TATCTGACTCCTTCTTTGCCTCAT")
 
 def binary_to_nucleotide (num):
     nucleotide = ""
+    #go through string by pairs, checking what each binary pair should match 
+    #with what dna base
     for i in range (0, len(num), 2):
         pair = num[i:i+2]
         if pair == "00":
@@ -31,5 +35,3 @@ def binary_to_nucleotide (num):
             nucleotide += "G"
     return (nucleotide)
 
-encode_sequence ("Frieza")
-#print(format(97,'08b'))
