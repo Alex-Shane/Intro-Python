9854ebcb8dbf06de43db12240127da509df227fb
Alex-Shane <ashane1230@gmail.com>
2023-01-07 15:03:43 -0800

    Decode algorithm working

--- Encryption #2/dna_info.py
+++ Encryption #2/dna_info.py
@@ -8,6 +8,9 @@ Created on Fri Jan  6 17:32:35 2023
 
 #Alex Shane, ashane4
 
+dna_dict = {"00":"A", "01":"T", "10":"C", "11":"G"}
+dna_dict_reversed_keys = {"A":"00", "T":"01", "C":"10", "G":"11"}
+
 def encode_sequence (message):
     encoded = ""
     #loop through every letter in message, turning it into a set of 4 dna bases
@@ -21,17 +24,23 @@ def encode_sequence (message):
 
 def binary_to_nucleotide (num):
     nucleotide = ""
-    #go through string by pairs, checking what each binary pair should match 
-    #with what dna base
+    #go through string by pairs, and add corresponding dna base to result string
     for i in range (0, len(num), 2):
         pair = num[i:i+2]
-        if pair == "00":
-            nucleotide += "A"
-        elif pair == "01":
-            nucleotide += "T"
-        elif pair == "10":
-            nucleotide += "C"
-        else:
-            nucleotide += "G"
+        nucleotide += dna_dict[pair]
     return (nucleotide)
+
+def decode_sequence (dna):
+    decoded = ""
+    for k in range (0,len(dna),4):
+        bases = dna[k:k+4]
+        decoded += nucleotides_to_letter (bases)
+    print (decoded)
+
+def nucleotides_to_letter (bases):
+    bin_string = ""
+    for i in range (4):
+        bin_string += dna_dict_reversed_keys [bases[i]]
+    return (chr(int(bin_string, 2)))
+
 
